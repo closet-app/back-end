324b055a4be45ea3bf1ab005b8d05303cb75a1f5
ShariqAli-Dev <shariqali.dev@gmail.com>
2022-07-25 17:50:13 -0400

    added typeDefs and resolvers for ApolloServerg

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-const {} = require('apollo-sever');
+const { ApolloServer, gql } = require('apollo-server');
+
+// String, Int, Float, Boolen, Id
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => {
+      return 'World';
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log('Server is ready at', url);
+});

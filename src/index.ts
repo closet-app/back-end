dbb37b8fd16517d5fd2be8f4ca5d79e1f5f1bc58
shariqali-dev <shariqali.dev@gmail.com>
2022-08-05 00:56:54 -0400

    express sanity check initial setup

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,20 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 // import { Review } from "./entities/Reviews";
 import microConfig from "./mikro-orm.config";
+import express from "express";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
   // const fork = orm.em.fork();
 
-  // const review = fork.create(Review, { title: "my first review 323" });
-  // await fork.persistAndFlush(review);
-
-  // const reviews = await fork.find(Review, {});
-  // console.log(reviews);
+  const app = express();
+  app.get("/", (_, res) => {
+    res.json({ message: "hello world" });
+  });
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
 };
 
 main().catch((err) => {

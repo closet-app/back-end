e9eb19747771739cae431df76b728431db402377
ShariqAli-Dev <shariqali.dev@gmail.com>
2022-08-05 11:48:34 -0400

    implelted CRU features, missing delete

--- src/resolvers/review.ts
+++ src/resolvers/review.ts
@@ -1,4 +1,4 @@
-import { Ctx, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Int, Mutation, Query, Resolver } from "type-graphql";
 import { Review } from "../entities/Review";
 import { MyContext } from "../types";
 
@@ -7,5 +7,40 @@ export class ReviewResolver {
   @Query(() => [Review])
   posts(@Ctx() { em }: MyContext): Promise<Review[]> {
     return em.find(Review, {});
+  }
+
+  @Query(() => Review, { nullable: true })
+  post(
+    @Arg("id") id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Review | null> {
+    return em.findOne(Review, { id });
+  }
+
+  @Mutation(() => Review)
+  async createReview(
+    @Arg("title") title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Review> {
+    const review = em.create(Review, { title });
+    await em.persistAndFlush(review);
+    return review;
+  }
+
+  @Mutation(() => Review, { nullable: true })
+  async updateReview(
+    @Arg("id") id: number,
+    @Arg("title", () => String, { nullable: true }) title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Review | null> {
+    const review = await em.findOne(Review, { id });
+    if (!review) {
+      return null;
+    }
+    if (typeof title !== "undefined") {
+      review.title = title;
+    }
+
+    return review;
   }
 }

f4539113ebe9ad249de73b81a7a374cea101859a
ChrisJRamirez <chrisjamesramirez@gmail.com>
2022-08-07 16:34:19 -0400

    installed .env

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
+require("dotenv").config();
 export const __prod__ = process.env.NODE_ENV === "production";
 export const __password__ = process.env.PASSWORD || "password";
 export const __tokenSecret__ = process.env.TOKEN_SECRET || "is a secret to all";
